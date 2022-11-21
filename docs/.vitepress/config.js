3aac9e51f9d998793a5c4ab38365931cb5a401a4
Daniele Tabanella <daniele.tabanella@gmail.com>
2022-11-21 09:29:10 +0100

    feat(add blog): add blog

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,12 @@
 export default {
+  base: '/',
   title: 'VitePress',
   description: 'Just playing around.',
-  base: '/'
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Blog', link: '/articles/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+  },
 }
